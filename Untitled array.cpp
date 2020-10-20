12c3757c2c6a070814bee5c46a87eb73eb1f76e6
nasiralibaloch <70817789+nasiralibaloch@users.noreply.github.com>
2020-10-20 23:33:43 +0500

    Add files via upload

---
+++ Untitled array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int no[5],i,sum=0;
+	cout<<"enter the five numbrs"<<endl;
+	{
+		for(i=0;i<5;i++)
+		{
+			cin>>no[i];
+			
+		}
+		for(i=0;i<5;i++)
+		{
+			cout<<no[i];
+	    	sum=sum+i;
+	    }
+    }
+	return 0;
+}
